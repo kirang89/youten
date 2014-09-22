f50a22fa8971711211c6683950f5e90b544e5527
kirang89 <kiran.daredevil@gmail.com>
2014-09-22 19:08:34 +0530

    Add model for Snippet

---
+++ api/helpers.py
@@ -0,0 +1,33 @@
+import datetime
+
+
+class Serializer(object):
+    """Mixin for retrieving public fields of SQLAlchemy models
+    in json-compatible format"""
+    __public__ = None
+
+    def get_public(self, exclude=(), extra=()):
+        "Returns model's PUBLIC data for jsonify"
+        data = {}
+        keys = self._sa_instance_state.attrs.items()
+        public = self.__public__ + extra if self.__public__ else extra
+        for k, field in keys:
+            if public and k not in public: continue
+            if k in exclude: continue
+            value = self._serialize(field.value)
+            if value:
+                data[k] = value
+        return data
+
+    @classmethod
+    def _serialize(cls, value, follow_fk=False):
+        if isinstance(value, datetime):
+            ret = value.isoformat()
+        elif hasattr(value, '__iter__'):
+            ret = [cls._serialize(v) for v in value]
+        elif Serializer in value.__class__.__bases__:
+            ret = value.get_public()
+        else:
+            ret = value
+
+        return ret
---
+++ api/models.py
@@ -0,0 +1,21 @@
+from api import db
+from api.helpers import Serializer
+import uuid
+
+
+class Snippet(db.Model, Serializer):
+    """A code snippet"""
+    __tablename__ = 'snippets'
+    id = db.Column(db.String, primary_key=True, unique=True)
+    code = db.Column(db.Text)
+    title = db.Column(db.Text)
+    language = db.Column(db.Text)
+
+    def __init__(self, title, language, code):
+        self.id = uuid.uuid4().hex
+        self.title = title
+        self.language = language
+        self.code = code
+
+    def __repr__(self):
+        return '<Snippet [{}]: {}>'.format(self.id, self.title)
